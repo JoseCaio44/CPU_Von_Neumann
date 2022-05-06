d4ddd2be029228860cc2337702303020cb6a2fcc
JoseCaio44 <jose.caio@academico.ifg.edu.br>
2022-05-06 13:12:19 -0300

    refatoracao iniciaal do codigo

--- GeradorBinarios.c
+++ GeradorBinarios.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-int main(void) {
+unsigned int mbr, mar = 0, imm, pc = 0, reg[8];
+unsigned char ir, ro0, ro1, e, l, g, memoria[154];
 
-	unsigned int mbr, mar, imm, pc, reg[8];
-	unsigned char ir, ro0, ro1, e, l, g, memoria[154];
-	
-	pc = 0x00;
-
+void ciclo() {
 	/* 
 	Valores carregados na memoria apenas para testes
 	um programa para fazer a codificação dos bits baseados em texto e necessario
 	*/
 	
-	memoria[0] = 0x13;
+/*	memoria[0] = 0x13;
 	memoria[1] = 0x20;
 	memoria[2] = 0x00;
 	memoria[3] = 0x04;
@@ -21,7 +20,7 @@ int main(void) {
 	memoria[4] = 0xaa;
 	memoria[5] = 0xbb;
 	memoria[6] = 0xcc;
-	memoria[7] = 0xdd;
+	memoria[7] = 0xdd;*/
 	
 	mar = pc;
 	mbr = memoria[mar++];
@@ -31,7 +30,7 @@ int main(void) {
 		
 	//printf("%x",ir);
 	//printf("%x",mbr);
-	///descodifica
+	///decodifica
 	///teste da fase inicial 
 	ir = (mbr >> 24);
 	//ir = memoria[0];
@@ -158,9 +157,216 @@ int main(void) {
 			reg[ro0] = (reg[ro0] >> imm);
 		};
 	};
+		
+}
+
+unsigned char *pt;
+
+void guardarMemoria(unsigned int mem ,unsigned int aux){
+    memoria[mem++] = (aux & 0xff000000) >> 24;
+    memoria[mem++] = (aux & 0x00ff0000) >> 16;
+    memoria[mem++] = (aux & 0x0000ff00) >> 8;
+    memoria[mem] = (aux & 0x0000ff);
+}
+
+void colocarInstru(int mem){
+    unsigned char *ponteiro, rg0, mine[8]; // aux recebe rg0
+    unsigned int count = 0, imediatoMem, palavra;
+
+    ponteiro = strtok(pt," ,");
+    while(ponteiro){
+        if(count == 0){
+            strcpy(mine,ponteiro);
+        }
+        if (count== 1){
+            rg0 = (int)strtol(ponteiro,NULL,16);
+        }
+        if(count == 2){
+            imediatoMem = (int)strtol(ponteiro,NULL,16);
+        }
+        ponteiro = strtok(NULL," ,r");
+        count++;
+    }
+    if(strcmp(mine,"nop")== 0) {
+        palavra = 1;
+        palavra = palavra << 24;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"add")== 0){
+        palavra = 2;
+        palavra = (palavra << 3) |rg0;
+        palavra = (palavra << 3) | imediatoMem;
+        palavra =  palavra << 18;
+        guardarMemoria(mem,palavra);
+    }else if(strcmp(mine,"sub")== 0) {
+        palavra = 3;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 3) | imediatoMem;
+        palavra = palavra << 18;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"mul")== 0) {
+        palavra = 4;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 3) | imediatoMem;
+        palavra = palavra << 18;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"div")== 0) {
+        palavra = 5;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 3) | imediatoMem;
+        palavra = palavra << 18;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"cmp")== 0) {
+        palavra = 6;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 3) | imediatoMem;
+        palavra = palavra << 18;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"movr")== 0) {
+        palavra = 7;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 3) | imediatoMem;
+        palavra = palavra << 18;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"and")== 0) {
+        palavra = 8;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 3) | imediatoMem;
+        palavra = palavra << 18;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"or")== 0) {
+        palavra = 9;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 3) | imediatoMem;
+        palavra = palavra << 18;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"xor")== 0) {
+        palavra = 10;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 3) | imediatoMem;
+        palavra = palavra << 18;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"not")== 0) {
+        palavra = 11;
+        palavra = ((palavra << 3)|rg0)<<21;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"je")== 0) {
+        palavra = 12;
+        palavra = (palavra << 24)| imediatoMem;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"jne")== 0) {
+        palavra = 13;
+        palavra = (palavra << 24)| imediatoMem;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"jl")== 0) {
+        palavra = 14;
+        palavra = (palavra << 24)| imediatoMem;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"jle")== 0) {
+        palavra = 15;
+        palavra = (palavra << 24)| imediatoMem;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"jg")== 0) {
+        palavra = 16;
+        palavra = (palavra << 24)| imediatoMem;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"jge")== 0) {
+        palavra = 17;
+        palavra = (palavra << 24)| imediatoMem;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"jmp")== 0) {
+        palavra = 18;
+        palavra = (palavra << 24)| imediatoMem;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"ld")== 0){
+        palavra = 19;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 21)| imediatoMem;
+        guardarMemoria(mem,palavra);
+    }else if(strcmp(mine,"st")== 0){
+        palavra = 20;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 21)| imediatoMem;
+        guardarMemoria(mem,palavra);
+    }else if(strcmp(mine,"movi")== 0) {
+        palavra = 21;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 21) | imediatoMem;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"addi")== 0) {
+        palavra = 22;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 21) | imediatoMem;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"subi")== 0) {
+        palavra = 23;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 21) | imediatoMem;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"muli")== 0) {
+        palavra = 24;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 21) | imediatoMem;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"divi")== 0) {
+        palavra = 25;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 21) | imediatoMem;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"lsh")== 0) {
+        palavra = 26;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 21) | imediatoMem;
+        guardarMemoria(mem, palavra);
+    }else if(strcmp(mine,"rsh")== 0) {
+        palavra = 27;
+        palavra = (palavra << 3) | rg0;
+        palavra = (palavra << 21) | imediatoMem;
+        guardarMemoria(mem, palavra);
+    }else{
+        palavra  = 0;
+        guardarMemoria(mem,palavra);
+    }
+}
+
+void lerArquivo(){
+
+    unsigned int mem, valor;
+    FILE *arq;
+    unsigned char tipo, linha[99];
+    unsigned int count = 0;
+    arq = fopen("tex.txt", "r");
+
+    while (fgets(linha, 99, arq) != NULL){
+        pt = strtok(linha, "; ");
+        mem = (int)strtol(pt,NULL,16);
+        while(pt){
+            if(count == 1){
+                tipo = *pt;
+            }else if(count == 2){
+                if(tipo == 0x64){
+                    valor = (int)strtol(pt,NULL,16);
+                    memoria[mem++] = (valor & 0xff000000) >> 24;
+                    memoria[mem++] = (valor & 0x00ff0000) >> 16;
+                    memoria[mem++] = (valor & 0x0000ff00) >> 8;
+                    memoria[mem]   = (valor & 0x000000ff);
+                }else{
+                    colocarInstru(mem);
+                }
+            }
+
+            pt = strtok(NULL ,";");
+            count++;
+        }
+        count = 0;
+    }
+
+    if (NULL == arq)
+        printf("Erro ao tentar abrir programa.txt \n");
+    fclose(arq);
+}
+
+int main (void) {
 	
-	//printf("%x", reg[1], "\n MBR: %x", mbr, "%x \n MAR: ", mar);
 	
-	// falta implemtar um controle de execução e imcrementação do PC
-	
+	return 0;
 }
