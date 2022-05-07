b3a3941eafdd949f138a57848cd83d1ccd53f114
JoseCaio44 <jose.caio@academico.ifg.edu.br>
2022-05-06 22:09:02 -0300

    finalização das intruções

--- GeradorBinarios.c
+++ GeradorBinarios.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 
 unsigned int mbr, mar = 0, imm, pc = 0, reg[8];
 unsigned char ir, ro0, ro1, e, l, g, memoria[154];
 
 void ciclo() {
-	/* 
-	Valores carregados na memoria apenas para testes
-	um programa para fazer a codificação dos bits baseados em texto e necessario
-	*/
 	
 /*	memoria[0] = 0x13;
 	memoria[1] = 0x20;
@@ -34,10 +29,12 @@ void ciclo() {
 	///teste da fase inicial 
 	ir = (mbr >> 24);
 	//ir = memoria[0];
-	printf("%x \n",mbr);
+	//printf("%x \n",mbr);
 	
 	if (ir == 0 || ir == 1) {
-		if (ir == 1){
+		if (ir == 0) {
+			//nao fazz nada
+		} else if (ir == 1){
 			pc += 4;
 		}	
 	}
@@ -46,41 +43,56 @@ void ciclo() {
 		ro1 = ((mbr & 0x1c0000) >> 18);
 		if (ir == 2) {
 			reg[ro0] = reg[ro0] + reg[ro1];
+			pc += 4;
 		} else if (ir == 3) {
 			reg[ro0] = reg[ro0] - reg[ro1];
+			pc += 4;
 		} else if (ir == 4) {
 			reg[ro0] = reg[ro0] * reg[ro1];
+			pc += 4;
 		} else if (ir == 5) {
 			reg[ro0] = reg[ro0] / reg[ro1];
+			pc += 4;
 		} else if (ir == 6) {
 			if (reg[ro0] == reg[ro1]){
 				e = 0x0001;
+				pc += 4;
 			} else {
 				e = 0x0000;
+				pc += 4;
 			};
 			if (reg[ro0] < reg[ro1]){
 				l = 0x0001;
+				pc += 4;
 			} else {
 				l = 0x0000;
+				pc += 4;
 			};
 			if (reg[ro0] > reg[ro1]){
 				g = 0x0001;
+				pc += 4;
 			} else {
 				g = 0x0000;
+				pc += 4;
 			};
 		} else if (ir == 7) {
 			reg[ro0] = reg[ro1];
+			pc += 4;
 		} else if (ir == 8) {
 			reg[ro0] = (reg[ro0] & reg[ro1]);
+			pc += 4;
 		} else if (ir == 9) {
 			reg[ro0] = (reg[ro0] | reg[ro1]);
+			pc += 4;
 		} else if (ir == 10) {
 			reg[ro0] = (reg[ro0] ^ reg[ro1]);
-		}					
+			pc += 4;
+		};					
 	}
 	else if (ir == 11) {
 		ro0 = ((mbr & 0xe00000) >> 21);
 		reg[ro0] = !reg[ro0];
+		pc += 4;
 	}
 	else if (ir >= 12 && ir <= 18){
 		mar = (mbr & 0x1fffff);
@@ -123,16 +135,21 @@ void ciclo() {
 		//printf("%d \n", ro0);
 		
 		if (ir == 19) {
-			printf("entrei no segundo \n");
+			//printf("entrei no segundo \n");
 			mbr = memoria[mar++];
 			mbr = (mbr << 8) | memoria[mar++];
 			mbr = (mbr << 8) | memoria[mar++];
 			mbr = (mbr << 8) | memoria[mar++];
 			reg[ro0] = mbr;
-			//pc += 4;
-			printf("%x", reg[ro0]);
+			pc += 4;
+			//printf("%x", reg[ro0], "\n");
 		} else if (ir == 20) {
-			
+			mbr = reg[ro0];
+			memoria[mar++] = (mbr >> 24);
+			memoria[mar++] = (mbr >> 16);
+			memoria[mar++] = (mbr >> 8);
+			memoria[mar++] = mbr;
+			pc += 4;
 		};
 		
 		//ro0 = (mbr & 0x00e00000) >> 21;
@@ -143,18 +160,25 @@ void ciclo() {
 		
 		if (ir == 21) {
 			reg[ro0] = imm;
+			pc += 4;
 		} else if (ir == 22) {
 			reg[ro0] = (reg[ro0] + imm);
+			pc += 4;
 		} else if (ir == 23) {
 			reg[ro0] = (reg[ro0] - imm);
+			pc += 4;
 		} else if (ir == 24) {
 			reg[ro0] = (reg[ro0] * imm);
+			pc += 4;
 		} else if (ir == 25) {
 			reg[ro0] = (reg[ro0] / imm);
+			pc += 4;
 		} else if (ir == 26) {
 			reg[ro0] = (reg[ro0] << imm);
+			pc += 4;
 		} else if (ir == 27) {
 			reg[ro0] = (reg[ro0] >> imm);
+			pc += 4;
 		};
 	};
 		
@@ -191,138 +215,138 @@ void colocarInstru(int mem){
         palavra = 1;
         palavra = palavra << 24;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"add")== 0){
+    } else if(strcmp(mine,"add")== 0){
         palavra = 2;
         palavra = (palavra << 3) |rg0;
         palavra = (palavra << 3) | imediatoMem;
         palavra =  palavra << 18;
         guardarMemoria(mem,palavra);
-    }else if(strcmp(mine,"sub")== 0) {
+    } else if(strcmp(mine,"sub")== 0) {
         palavra = 3;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 3) | imediatoMem;
         palavra = palavra << 18;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"mul")== 0) {
+    } else if(strcmp(mine,"mul")== 0) {
         palavra = 4;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 3) | imediatoMem;
         palavra = palavra << 18;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"div")== 0) {
+    } else if(strcmp(mine,"div")== 0) {
         palavra = 5;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 3) | imediatoMem;
         palavra = palavra << 18;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"cmp")== 0) {
+    } else if(strcmp(mine,"cmp")== 0) {
         palavra = 6;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 3) | imediatoMem;
         palavra = palavra << 18;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"movr")== 0) {
+    } else if(strcmp(mine,"movr")== 0) {
         palavra = 7;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 3) | imediatoMem;
         palavra = palavra << 18;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"and")== 0) {
+    } else if(strcmp(mine,"and")== 0) {
         palavra = 8;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 3) | imediatoMem;
         palavra = palavra << 18;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"or")== 0) {
+    } else if(strcmp(mine,"or")== 0) {
         palavra = 9;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 3) | imediatoMem;
         palavra = palavra << 18;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"xor")== 0) {
+    } else if(strcmp(mine,"xor")== 0) {
         palavra = 10;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 3) | imediatoMem;
         palavra = palavra << 18;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"not")== 0) {
+    } else if(strcmp(mine,"not")== 0) {
         palavra = 11;
         palavra = ((palavra << 3)|rg0)<<21;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"je")== 0) {
+    } else if(strcmp(mine,"je")== 0) {
         palavra = 12;
         palavra = (palavra << 24)| imediatoMem;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"jne")== 0) {
+    } else if(strcmp(mine,"jne")== 0) {
         palavra = 13;
         palavra = (palavra << 24)| imediatoMem;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"jl")== 0) {
+    } else if(strcmp(mine,"jl")== 0) {
         palavra = 14;
         palavra = (palavra << 24)| imediatoMem;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"jle")== 0) {
+    } else if(strcmp(mine,"jle")== 0) {
         palavra = 15;
         palavra = (palavra << 24)| imediatoMem;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"jg")== 0) {
+    } else if(strcmp(mine,"jg")== 0) {
         palavra = 16;
         palavra = (palavra << 24)| imediatoMem;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"jge")== 0) {
+    } else if(strcmp(mine,"jge")== 0) {
         palavra = 17;
         palavra = (palavra << 24)| imediatoMem;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"jmp")== 0) {
+    } else if(strcmp(mine,"jmp")== 0) {
         palavra = 18;
         palavra = (palavra << 24)| imediatoMem;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"ld")== 0){
+    } else if(strcmp(mine,"ld")== 0){
         palavra = 19;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 21)| imediatoMem;
         guardarMemoria(mem,palavra);
-    }else if(strcmp(mine,"st")== 0){
+    } else if(strcmp(mine,"st")== 0){
         palavra = 20;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 21)| imediatoMem;
         guardarMemoria(mem,palavra);
-    }else if(strcmp(mine,"movi")== 0) {
+    } else if(strcmp(mine,"movi")== 0) {
         palavra = 21;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 21) | imediatoMem;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"addi")== 0) {
+    } else if(strcmp(mine,"addi")== 0) {
         palavra = 22;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 21) | imediatoMem;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"subi")== 0) {
+    } else if(strcmp(mine,"subi")== 0) {
         palavra = 23;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 21) | imediatoMem;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"muli")== 0) {
+    } else if(strcmp(mine,"muli")== 0) {
         palavra = 24;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 21) | imediatoMem;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"divi")== 0) {
+    } else if(strcmp(mine,"divi")== 0) {
         palavra = 25;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 21) | imediatoMem;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"lsh")== 0) {
+    } else if(strcmp(mine,"lsh")== 0) {
         palavra = 26;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 21) | imediatoMem;
         guardarMemoria(mem, palavra);
-    }else if(strcmp(mine,"rsh")== 0) {
+    } else if(strcmp(mine,"rsh")== 0) {
         palavra = 27;
         palavra = (palavra << 3) | rg0;
         palavra = (palavra << 21) | imediatoMem;
         guardarMemoria(mem, palavra);
-    }else{
+    } else{
         palavra  = 0;
         guardarMemoria(mem,palavra);
     }
@@ -367,6 +391,40 @@ void lerArquivo(){
 
 int main (void) {
 	
+	lerArquivo();
+	
+	int teste = 0;
+	int aux = 0;
+	
+	while (aux != 3){
+		printf(" Aperte 1 para executar, 2 para mostrar a memoria, 3 para finalizar");
+		scanf("%d", &aux);
+		
+		if (aux == 1) {
+			ciclo();
+			printf("MBR: %x MAR: %x  IMM: %x PC: %x \n",mbr, mar, imm,pc);
+			printf("REG1: %x REG2: %x REG3: %x  REG4: %x \n", reg[0], reg[1], reg[2], reg[3]);
+			printf("REG5: %x  REG6: %x  REG7: %x  REG8: %x \n", reg[4], reg[5], reg[6], reg[7]);
+			printf("IR: %x  RO0: %x  RO1: %x  E: %x  L: %x G: %x \n", ir, ro0, ro1, e, l, g);
+			if (ir == 0) {
+				while(teste < 154){
+					printf("Posicao %d : %x \n", teste, memoria[teste++]);
+				};
+				break;
+			}
+		} else if (aux == 2) {
+			while(teste < 154){
+				printf("Posicao %d : %x \n", teste, memoria[teste++]);
+			};
+		} else {
+			break;
+		}
+	};
+
+	printf("MBR: %x MAR: %x  IMM: %x PC: %x \n",mbr, mar, imm,pc);
+	printf("REG1: %x REG2: %x REG3: %x  REG4: %x \n", reg[0], reg[1], reg[2], reg[3]);
+	printf("REG5: %x  REG6: %x  REG7: %x  REG8: %x \n", reg[4], reg[5], reg[6], reg[7]);
+	printf("IR: %x  RO0: %x  RO1: %x  E: %x  L: %x G: %x \n", ir, ro0, ro1, e, l, g);
 	
 	return 0;
 }
